4cfb86ee8f3663028cbd5a4cded87475599161b5
Wivex <wivexorium@gmail.com>
2015-09-17 22:31:26 +0300

    Wivex first commit
    test description

---
+++ Source/ResearchMods/TechUpgrades.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using RimWorld;
+using Verse;
+
+namespace GlobalMod
+{
+    public static class TechUpgrades
+    {
+        public static void ToMedieval()
+        {
+            FactionDef colony = DefDatabase<FactionDef>.GetNamed("Colony", true);
+            colony.techLevel = TechLevel.Medieval;
+        }
+
+    }
+}
