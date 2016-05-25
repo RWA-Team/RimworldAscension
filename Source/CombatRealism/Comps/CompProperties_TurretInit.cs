2d23f61a8318792b47fddd142f837da1251a3213
Ratysz <ratysz@gmail.com>
2016-05-25 20:41:32 +0300

    Combat Realism, straight port (compiles).

---
+++ Source/CombatRealism/Comps/CompProperties_TurretInit.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RimWorld;
+using Verse;
+using UnityEngine;
+
+namespace RA.CR
+{
+    public class CompProperties_TurretInit : CompProperties
+    {
+        public CompProperties_TurretInit()
+        {
+            this.compClass = typeof(CompTurretInit);
+        }
+    }
+}
---
+++ Source/CombatRealism/RecoilPattern.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RA.CR
+{
+    public enum RecoilPattern : byte
+    {
+        None,
+        Regular,
+        Mounted
+    }
+}
