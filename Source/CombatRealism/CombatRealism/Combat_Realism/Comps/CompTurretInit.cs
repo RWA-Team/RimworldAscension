0e9559982df9b4d82f5be61a289d656b3ec9dc5a
Ratysz <ratysz@gmail.com>
2016-05-25 21:03:50 +0300

    Plain copy.

---
+++ Source/CombatRealism/CombatRealism/Combat_Realism/Comps/CompTurretInit.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using RimWorld;
+using Verse;
+
+namespace Combat_Realism
+{
+    public class CompTurretInit : ThingComp
+    {
+        public CompProperties_TurretInit Props
+        {
+            get
+            {
+                return (CompProperties_TurretInit)this.props;
+            }
+        }
+        public Thing gun;
+
+        public override void Initialize(CompProperties props)
+        {
+            base.Initialize(props);
+            LongEventHandler.ExecuteWhenFinished(InitTurret);
+        }
+
+        private void InitTurret()
+        {
+            Building_TurretGunCR turret = parent as Building_TurretGunCR;
+            if (turret != null && turret.gun == null)
+            {
+                gun = (Thing)ThingMaker.MakeThing(parent.def.building.turretGunDef);
+                turret.gun = gun;
+            }
+        }
+
+        /*public override void PostSpawnSetup()
+        {
+            base.PostSpawnSetup();
+            //It just needed first time.
+            if (gun != null)
+            {
+                gun.Destroy();
+                gun = null;
+            }
+        }*/
+    }
+}
---
+++ Source/CombatRealism/CombatRealism/Combat_Realism/Verbs/VerbPropertiesCR.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using RimWorld;
+using Verse;
+using UnityEngine;
+
+namespace Combat_Realism
+{
+    public class VerbPropertiesCR : VerbProperties
+    {
+        public RecoilPattern recoilPattern = RecoilPattern.None;
+        public float recoilAmount = 0;
+        public float indirectFirePenalty = 0;
+        public float meleeArmorPenetration = 0;
+    }
+}
