eeebd72b9f5bf73292d8010bfc7aa5213ea242b4
joshwoo70 <joshwoo69@hotmail.sg>
2016-06-26 17:06:41 +0800

    Added AssemblyInfo.cs

---
+++ Source/RA/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Rimworld Ascension")]
+[assembly: AssemblyDescription("RA .dll file (If found in the wild report it back to wivex at ludeon.com/forum/)")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("RWA-Team")]
+[assembly: AssemblyProduct("RimworldAscension.Properties")]
+[assembly: AssemblyCopyright("(c) RWA Team 2015")]
+[assembly: AssemblyTrademark("NOT APPLICABLE")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e617601f-68ef-4fe1-9f91-baa08456c17c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
