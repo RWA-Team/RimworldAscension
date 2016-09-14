e07d6ea895d74ef78d72344598a3e0af4e51848d
Wivex <wivexorium@gmail.com>
2016-09-14 10:27:33 +0300

    14.09 10:27

--- Source/RA/StatWorkers/RA_StatWorker_MarketValue.cs
+++ Source/RA/StatWorkers/RA_StatWorker_MarketValue.cs
@@ -5,15 +5,26 @@ namespace RA
 {
     public class RA_StatWorker_MarketValue : StatWorker_MarketValue
     {
+        public static bool once = false;
         public override float GetValueUnfinalized(StatRequest req, bool applyPostProcess = true)
         {
             // market value for crafted things
-            var compCraftedValue = req.Thing?.TryGetComp<CompCraftedValue>();
-            if (compCraftedValue != null)
+            if (req.HasThing)
             {
-                return compCraftedValue.marketValue != -1
-                    ? compCraftedValue.marketValue
-                    : compCraftedValue.PredictMarketValue();
+                var compCraftedValue = req.Thing?.TryGetComp<CompCraftedValue>();
+                if (compCraftedValue != null)
+                {
+                    return compCraftedValue.marketValue != -1
+                        ? compCraftedValue.marketValue
+                        : compCraftedValue.PredictMarketValue();
+                }
+            }
+            else if (Current.ProgramState != ProgramState.Entry)
+            {
+                var def = req.Def as ThingDef;
+                var compProps = def?.CompDefFor<CompCraftedValue>() as CompCraftedValue_Properties;
+                if (compProps != null)
+                    return CompCraftedValue.PredictBaseMarketValue(def, compProps);
             }
             return base.GetValueUnfinalized(req, applyPostProcess);
         }
